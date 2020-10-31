0316168f2cb878bc7bd51ad225dec5c707932272
Oleh Ilchyshyn <olehilchyshyn@MacBook-Air-Oleh.local>
2020-11-01 00:54:08 +0200

    Completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webdrafft-angular';
+
+  cars: any[] = [
+    {name: 'Audi', year: 2018},
+    {name: 'BMW', year: 2015},
+    {name: 'Mercedes', year: 2016}
+  ];
+
+  car = 'Ford';
+
+  elementStyles: any = {
+    color: 'green',
+    opacity: 0.5
+  };
 }
